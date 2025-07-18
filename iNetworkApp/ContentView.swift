0f2c3af9aeda1dc101b30b230bdec9360faa4d78
Saravanakumar S <saravanakumars@equalexperts.com>
2025-07-18 14:13:37 +0530

    Initial Commit

---
+++ iNetworkApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iNetworkApp
+//
+//  Created by Saravanakumar Selladurai on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iNetworkApp/iNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNetworkAppApp.swift
+//  iNetworkApp
+//
+//  Created by Saravanakumar Selladurai on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
